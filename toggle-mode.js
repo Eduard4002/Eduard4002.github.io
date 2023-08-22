2561a1c386a1b118482c15b494c96757bf277275
Eduard4002 <mihiceduard@gmail.com>
2023-08-22 19:54:47 +0200

    Responsive desing finished

--- toggle-mode.js
+++ toggle-mode.js
@@ -1,11 +1,19 @@
 
 const button = document.querySelector('.mode-toggle');
 const icon = document.querySelector('.fa-moon');
+const sideProfile = document.querySelector('.sideProfile');
+const hasDarkShadow = sideProfile.querySelector('.dark-shadow');
+console.log(sideProfile);
+
 var currentTheme = localStorage.getItem('theme') || 'dark';
+
 document.documentElement.setAttribute('data-theme', currentTheme);
 
 if (currentTheme === 'dark') {
   icon.classList.replace('fa-moon', 'fa-sun');
+  if(!hasDarkShadow){
+    sideProfile.classList.add('dark-shadow');
+  }
 }
 
 button.addEventListener('click', function () {
@@ -15,11 +23,16 @@ button.addEventListener('click', function () {
     localStorage.setItem('theme', 'dark');
     document.documentElement.setAttribute('data-theme', 'dark');
     icon.classList.replace('fa-moon', 'fa-sun');
+    
+    sideProfile.classList.remove('dark-shadow');
+    
    //document.querySelectorAll('.fa-sun').forEach(icon => icon.classList.replace('fa-sun', 'fa-moon'));
   } else {
     localStorage.setItem('theme', 'light');
     document.documentElement.setAttribute('data-theme', 'light');
     icon.classList.replace('fa-sun', 'fa-moon');
+    sideProfile.classList.add('dark-shadow');
+
     //document.querySelectorAll('.fa-moon').forEach(icon => icon.classList.replace('fa-moon', 'fa-sun'));
   }
 });
